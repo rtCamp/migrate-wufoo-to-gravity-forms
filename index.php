94e1583f96e5455e01950b4cfc00d4c9a278845d
Saurabh Shukla <saurabh.shukla@rtcamp.com>
2013-09-26 15:55:25 +0530

    fixed include path and more typos

--- index.php
+++ index.php
@@ -3,14 +3,14 @@
 /*
   Plugin Name: Wufoo to Gravity Importer
   Plugin URI: http://rtcamp.com/
-  Description: Wufoo Form to Gravity Forms migrator!
+  Description: Import Wufoo data to Gravity Forms
   Version: 1.0
   Author: rtCamp
   Author URI: http://rtcamp.com
  */
 if (!defined('RT_WUFOO_IMPORT_PAGE_SIZE'))
     define('RT_WUFOO_IMPORT_PAGE_SIZE', 25);
-require_once('lib/Wufoo-PHP-API-Wrapper/WufooApiWrapper.php');
+require_once('lib/Wufoo-API/WufooApiWrapper.php');
 require_once('rtWufooAPI.php');
 require_once('rtProgress.php');
 require_once('rtWufoo.php');
